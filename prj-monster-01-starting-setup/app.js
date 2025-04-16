c90df061c08f0a769ffb5a89741e92a44b25072e
Kingsley <kifeanyi216@gmail.com>
2025-04-16 14:23:17 +0100

    Attack Methods

---
+++ prj-monster-01-starting-setup/app.js
@@ -0,0 +1,25 @@
+function getRandomvalue(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const app = Vue.createApp({
+  data() {
+    return {
+      playersHealth: 100,
+      monstersHealth: 100,
+    };
+  },
+  methods: {
+    attackMonster() {
+      const attackValue = getRandomvalue(5, 12);
+      this.monstersHealth -= attackValue;
+      this.attackPlayer();
+    },
+    attackPlayer() {
+      const attackValue = getRandomvalue(8, 15);
+      this.playersHealth -= attackValue;
+    },
+  },
+});
+
+app.mount("#game");
