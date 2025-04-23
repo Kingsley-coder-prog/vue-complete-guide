f05f831e1a57708a8cb439ec05fbaafc1c93f471
Kingsley <kifeanyi216@gmail.com>
2025-04-23 09:39:04 +0100

    Intro to Props

--- vue-cli-01-a-new-vue-project/src/App.vue
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -2,8 +2,8 @@
     <section>
         <header><h1>My Friends</h1></header>
         <ul>
-            <friend-contact></friend-contact>
-            <friend-contact></friend-contact>
+            <friend-contact name="Manuel Lorenz" phone-number="1234 56789" email-address="manuel@localhost.com"></friend-contact>
+            <friend-contact name="Julie Jones" phone-number="0987 654321" email-address="julie@localhost.com"></friend-contact>
         </ul>
     </section>
 </template>
