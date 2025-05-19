2304f6b9345bf526769d778c6e5a9dd79bad4180
Kingsley <kifeanyi216@gmail.com>
2025-05-19 16:26:08 +0100

    Introducing Getters - A Better Way of Getting Data

---
+++ vuex-01-starting-setup/src/components/FavoriteValue.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+    <h3>{{ counter }}</h3>
+    <p>We do more...</p>
+  </main>
+</template>
+
+<script>
+export default {
+  computed: {
+    counter() {
+      return this.$store.getters.normalizedCounter;
+    },
+  },
+};
+</script>
