3d33e68382fe11a153bdf9f46a94ba78a8c48ac6
Kingsley <kifeanyi216@gmail.com>
2025-05-23 20:43:39 +0100

    Auth in Vue II

--- auth-01-starting-project/src/App.vue
+++ auth-01-starting-project/src/App.vue
@@ -1,12 +1,10 @@
 <template>
-  <main>
-    <the-header></the-header>
-    <router-view v-slot="slotProps">
-      <transition name="route" mode="out-in">
-        <component :is="slotProps.Component"></component>
-      </transition>
-    </router-view>
-  </main>
+  <the-header></the-header>
+  <router-view v-slot="slotProps">
+    <transition name="route" mode="out-in">
+      <component :is="slotProps.Component"></component>
+    </transition>
+  </router-view>
 </template>
 
 <script>
